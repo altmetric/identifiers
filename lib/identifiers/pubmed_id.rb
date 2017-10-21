eb1f39785c47b4d41f3df2991a8124e0aede8ecd
Maciej Gajewski <gajewski.mm@gmail.com>
2017-10-21 11:32:40 +0100

    Support extracting PubMed IDs from PubMed URL
    Why:
    
    We were only extracting PubMed IDs if they were surrounded by spaces.
    This was to reduce the number of false positives (extacting random
    numbers from DOIs for example).
    The side effect of that change was that we would miss totally valid
    PubMed IDs coming from PubMed URLs.
    
    This change addresses the need by:
    
    This adds a separate extraction logic for extracting PubMed IDs from
    PubMed URLs.

--- lib/identifiers/pubmed_id.rb
+++ lib/identifiers/pubmed_id.rb
@@ -1,7 +1,40 @@
 module Identifiers
   class PubmedId
     def self.extract(str)
-      str.to_s.scan(/(?<=^|[[:space:]])0*(?!0)(\d+)(?=$|[[:space:]])/).flatten
+      new(str).extract
+    end
+
+    attr_reader :pubmed_candidate
+    private :pubmed_candidate
+
+    URL_REGEX = %r{^https?://(?:www\.)?ncbi.nlm.nih.gov/pubmed/}
+
+    def initialize(pubmed_candidate)
+      @pubmed_candidate = pubmed_candidate.to_s
+    end
+
+    def extract
+      extract_from_number | extract_from_pubmed_url
+    end
+
+    private
+
+    def extract_from_pubmed_url
+      return [] unless contains_pubmed_id?
+
+      Array(pubmed_id_from_url)
+    end
+
+    def contains_pubmed_id?
+      pubmed_candidate =~ URL_REGEX
+    end
+
+    def pubmed_id_from_url
+      pubmed_candidate.sub(URL_REGEX, '')
+    end
+
+    def extract_from_number
+      pubmed_candidate.scan(/(?<=^|[[:space:]])0*(?!0)(\d+)(?=$|[[:space:]])/).flatten
     end
   end
 end
--- spec/identifiers/pubmed_id_spec.rb
+++ spec/identifiers/pubmed_id_spec.rb
@@ -5,6 +5,30 @@ RSpec.describe Identifiers::PubmedId do
     expect(described_class.extract("123\n456")).to contain_exactly('123', '456')
   end
 
+  it 'extracts PubMed IDs from a PubMed url with www' do
+    url = "http://www.ncbi.nlm.nih.gov/pubmed/123456"
+
+    expect(described_class.extract(url)).to contain_exactly("123456")
+  end
+
+  it 'extracts PubMed IDs from a PubMed url with www and https' do
+    url = "https://www.ncbi.nlm.nih.gov/pubmed/123456"
+
+    expect(described_class.extract(url)).to contain_exactly("123456")
+  end
+
+  it 'extracts PubMed IDs from a PubMed url without www' do
+    url = "http://ncbi.nlm.nih.gov/pubmed/123456"
+
+    expect(described_class.extract(url)).to contain_exactly("123456")
+  end
+
+  it 'extracts PubMed IDs from a PubMed url without www but with https' do
+    url = "https://ncbi.nlm.nih.gov/pubmed/123456"
+
+    expect(described_class.extract(url)).to contain_exactly("123456")
+  end
+
   it 'does not return outputs with PubMed IDs in DOIs' do
     str = "10.1038/nplants.2015.3\n10.1126/science.286.5445.1679e"
 
