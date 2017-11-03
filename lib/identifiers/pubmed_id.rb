5bfa67fab8cea2c1473d0b261a1cdc406bbfebc3
Jonathan Hernandez <jbilbo@gmail.com>
2017-11-03 16:43:35 +0100

    Skip query params and support multiple occurrences
    Use `scan` to retrieve all PMIDs occurrences from URLs. Also, avoid
    all query/hash parameters the URL might have.

--- lib/identifiers/pubmed_id.rb
+++ lib/identifiers/pubmed_id.rb
@@ -1,40 +1,9 @@
 module Identifiers
   class PubmedId
     def self.extract(str)
-      new(str).extract
-    end
-
-    attr_reader :pubmed_candidate
-    private :pubmed_candidate
-
-    URL_REGEX = %r{^https?://(?:www\.)?ncbi.nlm.nih.gov/pubmed/}
-
-    def initialize(pubmed_candidate)
-      @pubmed_candidate = pubmed_candidate.to_s
-    end
-
-    def extract
-      extract_from_number | extract_from_pubmed_url
-    end
-
-    private
-
-    def extract_from_pubmed_url
-      return [] unless contains_pubmed_id?
-
-      Array(pubmed_id_from_url)
-    end
-
-    def contains_pubmed_id?
-      pubmed_candidate =~ URL_REGEX
-    end
-
-    def pubmed_id_from_url
-      pubmed_candidate.sub(URL_REGEX, '')
-    end
-
-    def extract_from_number
-      pubmed_candidate.scan(/(?<=^|[[:space:]])0*(?!0)(\d+)(?=$|[[:space:]])/).flatten
+      str = str.to_s
+      str.scan(/(?<=^|[[:space:]])0*(?!0)(\d+)(?=$|[[:space:]])/).flatten |
+        str.scan(%r{https?://(?:www\.)?ncbi.nlm.nih.gov/pubmed/(\d+)}).flatten
     end
   end
 end
--- spec/identifiers/pubmed_id_spec.rb
+++ spec/identifiers/pubmed_id_spec.rb
@@ -5,28 +5,46 @@ RSpec.describe Identifiers::PubmedId do
     expect(described_class.extract("123\n456")).to contain_exactly('123', '456')
   end
 
-  it 'extracts PubMed IDs from a PubMed url with www' do
-    url = "http://www.ncbi.nlm.nih.gov/pubmed/123456"
+  it 'extracts PubMed IDs from a PubMed URL with www' do
+    url = 'http://www.ncbi.nlm.nih.gov/pubmed/123456'
 
-    expect(described_class.extract(url)).to contain_exactly("123456")
+    expect(described_class.extract(url)).to contain_exactly('123456')
   end
 
-  it 'extracts PubMed IDs from a PubMed url with www and https' do
-    url = "https://www.ncbi.nlm.nih.gov/pubmed/123456"
+  it 'extracts PubMed IDs from a PubMed URL with www and https' do
+    url = 'https://www.ncbi.nlm.nih.gov/pubmed/123456'
 
-    expect(described_class.extract(url)).to contain_exactly("123456")
+    expect(described_class.extract(url)).to contain_exactly('123456')
   end
 
-  it 'extracts PubMed IDs from a PubMed url without www' do
-    url = "http://ncbi.nlm.nih.gov/pubmed/123456"
+  it 'extracts PubMed IDs from a PubMed URL without www' do
+    url = 'http://ncbi.nlm.nih.gov/pubmed/123456'
 
-    expect(described_class.extract(url)).to contain_exactly("123456")
+    expect(described_class.extract(url)).to contain_exactly('123456')
   end
 
-  it 'extracts PubMed IDs from a PubMed url without www but with https' do
-    url = "https://ncbi.nlm.nih.gov/pubmed/123456"
+  it 'extracts PubMed IDs from a PubMed URL without www but with https' do
+    url = 'https://ncbi.nlm.nih.gov/pubmed/123456'
 
-    expect(described_class.extract(url)).to contain_exactly("123456")
+    expect(described_class.extract(url)).to contain_exactly('123456')
+  end
+
+  it 'extracts PubMed IDs from a PubMed URL with hash parameters' do
+    url = 'https://www.ncbi.nlm.nih.gov/pubmed/123456#cm6191871_69589'
+
+    expect(described_class.extract(url)).to contain_exactly('123456')
+  end
+
+  it 'extracts PubMed IDs from a PubMed URL with query parameters' do
+    url = 'https://www.ncbi.nlm.nih.gov/pubmed/123456?hi=hello&goodbye=bye'
+
+    expect(described_class.extract(url)).to contain_exactly('123456')
+  end
+
+  it 'extracts both number and URLs PubMed IDs' do
+    url = 'PubMed ID: 112233 another: https://www.ncbi.nlm.nih.gov/pubmed/123456'
+
+    expect(described_class.extract(url)).to contain_exactly('112233', '123456')
   end
 
   it 'does not return outputs with PubMed IDs in DOIs' do
@@ -39,15 +57,15 @@ RSpec.describe Identifiers::PubmedId do
     expect(described_class.extract("0000010203\n000456000")).to contain_exactly('10203', '456000')
   end
 
-  it 'does not consider 0 as a valid Pubmed ID' do
-    expect(described_class.extract("00000000")).to be_empty
+  it 'does not consider 0 as a valid PubMed ID' do
+    expect(described_class.extract('00000000')).to be_empty
   end
 
   it 'extracts PubMed IDs separated by Unicode whitespace' do
     expect(described_class.extract('123 456')).to contain_exactly('123', '456')
   end
 
-  it 'considers Fixnum as potential PubmedIds too' do
+  it 'considers Fixnum as potential PubMed IDs too' do
     expect(described_class.extract(123)).to contain_exactly('123')
   end
 end
