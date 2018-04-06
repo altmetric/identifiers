e9fae4cf14a46632a6a50e1601eea3710f783952
Paul Mucur <paul@altmetric.com>
2018-04-06 15:19:08 +0100

    Support wider variety of DOIs
    By using Crossref's API, we collected a sample of nearly 1,000 DOIs and
    tested our extraction against them. Aside from DOIs ending in full stops
    (e.g. 10.17147/asui.), all tests now pass.

--- lib/identifiers/doi.rb
+++ lib/identifiers/doi.rb
@@ -2,25 +2,27 @@ module Identifiers
   class DOI
     REGEXP = %r{
       \b
-      10                                      # Directory indicator (always 10)
+      10                                        # Directory indicator (always 10)
       \.
       (?:
         # ISBN-A
-        97[89]\.                              # ISBN (GS1) Bookland prefix
-        \d{2,8}                               # ISBN registration group element and publisher prefix
-        /                                     # Prefix/suffix divider
-        \d{1,7}                               # ISBN title enumerator and check digit
+        97[89]\.                                # ISBN (GS1) Bookland prefix
+        \d{2,8}                                 # ISBN registration group element and publisher prefix
+        /                                       # Prefix/suffix divider
+        \d{1,7}                                 # ISBN title enumerator and check digit
         |
         # DOI
-        \d{4,9}                               # Registrant code
-        /                                     # Prefix/suffix divider
+        \d{4,9}                                 # Registrant code
+        /                                       # Prefix/suffix divider
         (?:
           # DOI suffix
-          [^[:space:]]+2-\#                   # Early Wiley suffix
+          [^[:space:]]+;2-[\#0-9a-z]            # Early Wiley suffix
           |
-          [^[:space:]]+\([^[:space:])]+\)     # Suffix ending in balanced parentheses
+          [^[:space:]]+                         # Suffix...
+          \([^[:space:])]+\)                    # Ending in balanced parentheses...
+          (?![^[:space:]\p{P}])                 # Not followed by more suffix or punctuation
           |
-          [^[:space:]]+(?![[:space:]])\p{^P}  # Suffix ending in non-punctuation
+          [^[:space:]]+(?![[:space:]])\p{^P}    # Suffix ending in non-punctuation
         )
       )
     }x
