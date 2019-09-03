f88b769e759da6035821aabee12a72679d7e2f12
Ana Castro <ana@altmetric.com>
2019-09-03 16:36:08 +0100

    Add strict mode to DOI.extract
    A very small percentage of DOIs have trailing `.`.
    Add an `options` parameter to `DOI.extract`, so that we can change the
    default behaviour and extract DOIs while keeping trailing `.`.
    After scanning a Crossref mirror, we only found around 5K DOIs ending in
    between 1 to 3 `.`. So this was the only scenario we've added to the
    regex, to try and keep it as strict as possible.
    
    There's also been a change to the Travis config. This was due to the
    release of Bundler 2.0, which dropped support for Ruby versions 2.2 and
    older. More can be found here:
    https://docs.travis-ci.com/user/languages/ruby/#bundler-20

--- spec/identifiers/doi_spec.rb
+++ spec/identifiers/doi_spec.rb
@@ -1,151 +1,195 @@
 require 'identifiers/doi'
 
 RSpec.describe Identifiers::DOI do
-  it 'extracts DOIs from a string' do
-    str = 'This is an example of a DOI: 10.1049/el.2013.3006'
+  OPTIONS = [{ strict: false }, { strict: true }].freeze
 
-    expect(described_class.extract(str)).to contain_exactly('10.1049/el.2013.3006')
+  def each_doi(file)
+    Pathname.new(__FILE__).join('..', '..', 'fixtures', file).each_line do |doi|
+      yield(doi.chomp!)
+    end
   end
 
-  it 'extracts DOIs from anywhere in a string' do
-    str = 'This is an example of a DOI - 10.1049/el.2013.3006 - which is entirely valid'
+  OPTIONS.each do |options|
+    context "when extracting with options set to #{options.inspect}" do
+      it 'extracts DOIs from a string' do
+        str = 'This is an example of a DOI: 10.1049/el.2013.3006'
 
-    expect(described_class.extract(str)).to contain_exactly('10.1049/el.2013.3006')
+        expect(described_class.extract(str, options)).to contain_exactly('10.1049/el.2013.3006')
+      end
+
+      it 'extracts DOIs from anywhere in a string' do
+        str = 'This is an example of a DOI - 10.1049/el.2013.3006 - which is entirely valid'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1049/el.2013.3006')
+      end
+
+      it 'downcases the DOIs extracted' do
+        str = 'This is an example of a DOI: 10.1097/01.ASW.0000443266.17665.19'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1097/01.asw.0000443266.17665.19')
+      end
+
+      it 'does not extract a PubMed ID' do
+        str = 'This is NOT a DOI: 123456'
+
+        expect(described_class.extract(str, options)).to be_empty
+      end
+
+      it 'returns no DOIs if given nothing' do
+        expect(described_class.extract(nil)).to be_empty
+      end
+
+      it 'extracts ISBN-As' do
+        str = 'This is an ISBN-A: 10.978.8898392/315'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.978.8898392/315')
+      end
+
+      it 'does not extract invalid ISBN-As' do
+        str = 'This is not an ISBN-A: 10.978.8898392/NotARealIsbnA'
+
+        expect(described_class.extract(str, options)).to be_empty
+      end
+
+      it 'retains closing parentheses that are part of the DOI' do
+        str = 'This is an example of a DOI: 10.1130/2013.2502(04)'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1130/2013.2502(04)')
+      end
+
+      it 'discards ellipses' do
+        str = 'This is an example of a DOI: 10.1130/2013.2502…'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1130/2013.2502')
+      end
+
+      it 'extracts old Wiley DOIs' do
+        str = 'This is an example of an old Wiley DOI: 10.1002/(SICI)1096-8644(199601)99:1<135::AID-AJPA8>3.0.CO;2-# 10.1002/(sici)1099-0690(199806)1998:6<1071::aid-ejoc1071>3.0.co;2-5'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1002/(sici)1096-8644(199601)99:1<135::aid-ajpa8>3.0.co;2-#', '10.1002/(sici)1099-0690(199806)1998:6<1071::aid-ejoc1071>3.0.co;2-5')
+      end
+
+      it 'does not extract a closing parenthesis if not part of the DOI' do
+        str = '(This is an example of a DOI: 10.1130/2013.2502)'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1130/2013.2502')
+      end
+
+      it 'discards trailing punctuation from old Wiley DOIs' do
+        str = 'This is an example of an old Wiley DOI: 10.1002/(SICI)1096-8644(199601)99:1<135::AID-AJPA8>3.0.CO;2-#",'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1002/(sici)1096-8644(199601)99:1<135::aid-ajpa8>3.0.co;2-#')
+      end
+
+      it 'discards trailing Unicode punctuation after balanced parentheses' do
+        str = 'This is an example of a DOI: 10.1130/2013.2502(04)…",'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1130/2013.2502(04)')
+      end
+
+      it 'discards contiguous trailing punctuation after unbalanced parentheses' do
+        str = '(This is an example of a DOI: 10.1130/2013.2502).",'
+
+        expect(described_class.extract(str, options)).to contain_exactly('10.1130/2013.2502')
+      end
+
+      it 'does not extract DOIs with purely punctuation suffixes' do
+        expect(described_class.extract('10.1130/!).",', options)).to be_empty
+      end
+
+      it 'extracts DOIs with emoji in them' do
+        expect(described_class.extract('10.1234/🐔💩123🐔🐔🐔123', options)).to contain_exactly('10.1234/🐔💩123🐔🐔🐔123')
+      end
+
+      it 'extracts DOIs separated by Unicode whitespace' do
+        expect(described_class.extract('10.1234/foo  10.1234/bar', options)).to contain_exactly('10.1234/foo', '10.1234/bar')
+      end
+
+      it 'does not extract DOIs with extra digits prefixed' do
+        expect(described_class.extract('110.1234/foo', options)).to be_empty
+      end
+
+      it 'extracts DOIs from a string with trailing closing parentheses' do
+        expect(described_class.extract('(10.1130/2013.2502(04))', options)).to contain_exactly('10.1130/2013.2502(04)')
+      end
+
+      it 'extracts DOIs from a string with multiple trailing closing parentheses' do
+        expect(described_class.extract('10.1130/2013.2502(04))))', options)).to contain_exactly('10.1130/2013.2502(04)')
+      end
+
+      it 'extracts DOIs with parentheses within the suffix' do
+        expect(described_class.extract('10.1016/0005-2744(70)90072-0', options)).to contain_exactly('10.1016/0005-2744(70)90072-0')
+      end
+
+      it 'extracts all DOIs from a Crossref sample' do
+        each_doi('dois.txt') { |doi|
+          expect(described_class.extract(doi, options)).to contain_exactly(doi)
+        }
+      end
+    end
   end
 
-  it 'downcases the DOIs extracted' do
-    str = 'This is an example of a DOI: 10.1097/01.ASW.0000443266.17665.19'
+  context 'when no options are provided' do
+    it 'discards trailing punctuation' do
+      str = 'This is an example of a DOI: 10.1130/2013.2502.'
 
-    expect(described_class.extract(str)).to contain_exactly('10.1097/01.asw.0000443266.17665.19')
+      expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
+    end
+
+    it 'discards multiple contiguous trailing punctuation' do
+      str = 'This is an example of a DOI: 10.1130/2013.2502...",'
+
+      expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
+    end
+
+    it 'discards trailing punctuation after balanced parentheses' do
+      str = 'This is an example of a DOI: This is an example of a DOI: 10.1130/2013.2502(04).'
+
+      expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502(04)')
+    end
+
+    it 'discards contiguous trailing punctuation after balanced parentheses' do
+      str = 'This is an example of a DOI: This is an example of a DOI: 10.1130/2013.2502(04).",'
+
+      expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502(04)')
+    end
+
+    it 'does not overflow when given lots of trailing punctuation' do
+      str = '10.1130/2013.2502' + ('.' * 10000)
+
+      expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
+    end
   end
 
-  it 'does not extract a PubMed ID' do
-    str = 'This is NOT a DOI: 123456'
+  context 'with strict mode on' do
+    it 'extracts DOIs ending with trailing periods' do
+      str = 'This is an example of a DOI: 10.1130/2013.2502...",'
 
-    expect(described_class.extract(str)).to be_empty
-  end
+      expect(described_class.extract(str, strict: true)).to contain_exactly('10.1130/2013.2502...')
+    end
 
-  it 'returns no DOIs if given nothing' do
-    expect(described_class.extract(nil)).to be_empty
-  end
+    it 'keeps trailing punctuation after balanced parentheses' do
+      str = 'This is an example of a DOI: This is an example of a DOI: 10.1130/2013.2502(04).'
 
-  it 'extracts ISBN-As' do
-    str = 'This is an ISBN-A: 10.978.8898392/315'
+      expect(described_class.extract(str, strict: true)).to contain_exactly('10.1130/2013.2502(04).')
+    end
 
-    expect(described_class.extract(str)).to contain_exactly('10.978.8898392/315')
-  end
+    it 'discards contiguous trailing punctuation after balanced parentheses' do
+      str = 'This is an example of a DOI: This is an example of a DOI: 10.1130/2013.2502(04).",'
 
-  it 'does not extract invalid ISBN-As' do
-    str = 'This is not an ISBN-A: 10.978.8898392/NotARealIsbnA'
+      expect(described_class.extract(str, strict: true)).to contain_exactly('10.1130/2013.2502(04).')
+    end
 
-    expect(described_class.extract(str)).to be_empty
-  end
+    it 'limits the trailing periods to 3' do
+      str = 'This is an example of a DOI: 10.1130/2013.2502.......'
 
-  it 'retains closing parentheses that are part of the DOI' do
-    str = 'This is an example of a DOI: 10.1130/2013.2502(04)'
+      expect(described_class.extract(str, strict: true)).to contain_exactly('10.1130/2013.2502...')
+    end
 
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502(04)')
-  end
-
-  it 'discards trailing punctuation' do
-    str = 'This is an example of a DOI: 10.1130/2013.2502.'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
-  end
-
-  it 'discards multiple contiguous trailing punctuation' do
-    str = 'This is an example of a DOI: 10.1130/2013.2502...",'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
-  end
-
-  it 'discards trailing Unicode punctuation' do
-    str = 'This is an example of a DOI: 10.1130/2013.2502…'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
-  end
-
-  it 'extracts old Wiley DOIs' do
-    str = 'This is an example of an old Wiley DOI: 10.1002/(SICI)1096-8644(199601)99:1<135::AID-AJPA8>3.0.CO;2-# 10.1002/(sici)1099-0690(199806)1998:6<1071::aid-ejoc1071>3.0.co;2-5'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1002/(sici)1096-8644(199601)99:1<135::aid-ajpa8>3.0.co;2-#', '10.1002/(sici)1099-0690(199806)1998:6<1071::aid-ejoc1071>3.0.co;2-5')
-  end
-
-  it 'does not extract a closing parenthesis if not part of the DOI' do
-    str = '(This is an example of a DOI: 10.1130/2013.2502)'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
-  end
-
-  it 'discards trailing punctuation from old Wiley DOIs' do
-    str = 'This is an example of an old Wiley DOI: 10.1002/(SICI)1096-8644(199601)99:1<135::AID-AJPA8>3.0.CO;2-#",'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1002/(sici)1096-8644(199601)99:1<135::aid-ajpa8>3.0.co;2-#')
-  end
-
-  it 'discards trailing punctuation after balanced parentheses' do
-    str = 'This is an example of a DOI: This is an example of a DOI: 10.1130/2013.2502(04).'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502(04)')
-  end
-
-  it 'discards contiguous trailing punctuation after balanced parentheses' do
-    str = 'This is an example of a DOI: This is an example of a DOI: 10.1130/2013.2502(04).",'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502(04)')
-  end
-
-  it 'discards trailing Unicode punctuation after balanced parentheses' do
-    str = 'This is an example of a DOI: 10.1130/2013.2502(04)…",'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502(04)')
-  end
-
-  it 'discards contiguous trailing punctuation after unbalanced parentheses' do
-    str = '(This is an example of a DOI: 10.1130/2013.2502).",'
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
-  end
-
-  it 'does not overflow when given lots of trailing punctuation' do
-    str = '10.1130/2013.2502' + ('.' * 10000)
-
-    expect(described_class.extract(str)).to contain_exactly('10.1130/2013.2502')
-  end
-
-  it 'does not extract DOIs with purely punctuation suffixes' do
-    expect(described_class.extract('10.1130/!).",')).to be_empty
-  end
-
-  it 'extracts DOIs with emoji in them' do
-    expect(described_class.extract('10.1234/🐔💩123🐔🐔🐔123')).to contain_exactly('10.1234/🐔💩123🐔🐔🐔123')
-  end
-
-  it 'extracts DOIs separated by Unicode whitespace' do
-    expect(described_class.extract('10.1234/foo  10.1234/bar')).to contain_exactly('10.1234/foo', '10.1234/bar')
-  end
-
-  it 'does not extract DOIs with extra digits prefixed' do
-    expect(described_class.extract('110.1234/foo')).to be_empty
-  end
-
-  it 'extracts DOIs from a string with trailing closing parentheses' do
-    expect(described_class.extract('10.1130/2013.2502(04))')).to contain_exactly('10.1130/2013.2502(04)')
-  end
-
-  it 'extracts DOIs from a string with multiple trailing closing parentheses' do
-    expect(described_class.extract('10.1130/2013.2502(04))))')).to contain_exactly('10.1130/2013.2502(04)')
-  end
-
-  it 'extracts DOIs with parentheses within the suffix' do
-    expect(described_class.extract('10.1016/0005-2744(70)90072-0')).to contain_exactly('10.1016/0005-2744(70)90072-0')
-  end
-
-  it 'extracts all DOIs from a Crossref sample' do
-    Pathname.new(__FILE__).join('..', '..', 'fixtures', 'dois.txt').each_line do |doi|
-      doi.chomp!
-
-      expect(described_class.extract(doi)).to contain_exactly(doi)
+    it 'extracts all DOIs from a Crossref sample, keeping the trailing periods' do
+      each_doi('strict_mode_dois.txt') { |doi|
+        expect(described_class.extract(doi, strict: true)).to contain_exactly(doi)
+      }
     end
   end
 end
